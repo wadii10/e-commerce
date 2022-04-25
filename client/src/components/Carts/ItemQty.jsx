fc7622a96e1c5e79efb9e6ae8dc9b2c5f040eb84
wadii10 <wadii.driss@gmail.com>
2022-04-25 10:48:31 +0100

    feat(update)

--- client/src/components/Carts/ItemQty.jsx
+++ client/src/components/Carts/ItemQty.jsx
@@ -1,19 +1,19 @@
+import React from 'react';
 import { IconButton } from '@mui/material';
-import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { decrementQty, incrementQty } from '../../redux/actions/actionCart';
 
 
 const ItemQty = ({ el }) => {
 
-  const [qty, setQty] = useState(el.qty);
+  
 
   const dispatch = useDispatch();
 
   return (
     <div className="ItemQty">
       {
-        qty > 0 ? <div><IconButton onClick={ () => dispatch(decrementQty(el))} ><i className="fa fa-minus-circle"></i></IconButton> <span >{el.qty}</span> <IconButton onClick={ () => dispatch(incrementQty(el))}><i className="fa fa-plus-circle"></i></IconButton></div> :
+        el.qty > 1 ? <div><IconButton onClick={ () => dispatch(decrementQty(el))} ><i className="fa fa-minus-circle"></i></IconButton> <span >{el.qty}</span> <IconButton onClick={ () => dispatch(incrementQty(el))}><i className="fa fa-plus-circle"></i></IconButton></div> :
           <div> <IconButton  ><i className="fa fa-minus-circle"></i></IconButton> <span >{el.qty}</span> <IconButton onClick={ () => dispatch(incrementQty(el))}><i className="fa fa-plus-circle"></i></IconButton></div>
       }
     </div>
